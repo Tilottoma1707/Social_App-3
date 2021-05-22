bef2c8f689eb951ba50fb3e6d945f21a18e8e90e
Tilottoma Bhattacharyya <62555308+Tilottoma1707@users.noreply.github.com>
2021-05-22 22:36:22 +0530

    Add files via upload

---
+++ Screens/NoticeScreen.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react'
+import{
+        View,
+        Text,
+        TouchableOpacity,
+        StyleSheet
+} from 'react-native'
+
+import HomeScreen from './HomeScreen'
+
+export default class NoticeScreen extends Component{
+render(){
+    return(
+    <View>
+        <Text>LATEST NOTICE</Text>
+    </View>    
+    )
+}
+
+
+
+
+
+
+}
